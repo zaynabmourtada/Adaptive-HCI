af1ae453185192ac7853c6ec838c407e138feb26
Deniz K Acikbas <denizkarya1999@outlook.com>
2025-01-20 11:53:09 -0500

    <01.19.2025 - DKA> Added 3D Pathing Feature with OpenGL ES

--- Xamera/settings.gradle.kts
+++ Xamera/settings.gradle.kts
@@ -16,5 +16,3 @@ dependencyResolutionManagement {
 rootProject.name = "Xamera"
 include(":app")
 include(":OpenCV-4.10.0")
-include(":unityLibrary")
-project(":unityLibrary").projectDir = file("unityLibrary")
