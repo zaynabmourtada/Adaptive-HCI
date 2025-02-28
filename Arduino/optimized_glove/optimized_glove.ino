cf95af7e49cfff7a437d59f79438e0f205293196
Soham Naik <sohamnaik15@gmail.com>
2025-02-27 23:02:17 -0500

    Arduino Update

--- Arduino/optimized_glove/optimized_glove.ino
+++ Arduino/optimized_glove/optimized_glove.ino
@@ -1,55 +1,34 @@
 // 2022/11/24
 // Written by Soham Naik - 11/08/2024
 // Modified by Zaynab Mourtada - 12/29/2024
-// Glove 1 - Single Light Source for OOK (On-Off Keying) Signal
-// Single node: Index finger connected to D3 (PWM-capable pin)
+// Glove 1 - Two Light Sources for OOK Signal
+// Example of toggling LED_1 and LED_2 simultaneously
 
-int indexFingerLED = 3;  // PWM-capable pin
+int LED_1 = 7; // Thumb
+int LED_2 = 4; // Pinky
 
-float shutterRate = 1000.0; // Default shutter rate in Hz
-float shutterPeriod;        // Shutter period in microseconds
-int pilotGap;               // Delay between each OOK signal
-int pulseWidth;             // Width of each OOK bit LIGHT band
-int gapWidth;               // Width of each OOK bit DARK band
+float shutterRate = 6000.0;  
+float shutterPeriod;
 
-// Splits the shutter period into ON time and OFF time
-void calculateTiming() {
-  shutterPeriod = (1.0 / shutterRate) * 1e6; // Converting shutter rate to microseconds
-  pulseWidth = shutterPeriod * 1.0;         // 50% ON
-  gapWidth = shutterPeriod * 1.0;           // 50% OFF
-  pilotGap = shutterPeriod * 7;
-}
+char USER_1[] = "10101010";
+char USER_2[] = "11110000";
 
-// Function to emit a gap between OOK signals
-void emitPilotGap() {
-  digitalWrite(indexFingerLED, LOW);    // Turn LED OFF
-  delayMicroseconds(pilotGap);
-}
-
-// Function to emit a symbol with a specified pattern
-void emitSymbol(const char* binaryPattern) {
-  for (int i = 0; binaryPattern[i] != '\0'; i++) {
-    if (binaryPattern[i] == '1') {
-      digitalWrite(indexFingerLED, HIGH);  // LED ON for "1" bit
-      delayMicroseconds(pulseWidth);
-    } else {
-      digitalWrite(indexFingerLED, LOW);   // LED OFF for "0" bit
-      delayMicroseconds(gapWidth);
-    }
-  }
-}
-
-// Function to generate the OOK signal
-void generateOOKSignal() {
-  //emitPilotGap();                     // Emit pilot gap
-  //emitSymbol("11110000");        // User_1
-  emitSymbol("10101010");          // User_2
-}
 void setup() {
-  pinMode(indexFingerLED, OUTPUT);
-  calculateTiming();
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  shutterPeriod = (1.0 / shutterRate) * 1e6;
+}
+
+void updateLED(int pin, int& index, const char* pattern) {
+  digitalWrite(pin, (pattern[index] == '1') ? HIGH : LOW);
+  index = (index + 1) % strlen(pattern);
 }
 
 void loop() {
-  generateOOKSignal();  // Continuously generate OOK signal
+  static int i1 = 0, i2 = 0;
+
+  updateLED(LED_1, i1, USER_1);
+  updateLED(LED_2, i2, USER_2);
+
+  delayMicroseconds(shutterPeriod);
 }
